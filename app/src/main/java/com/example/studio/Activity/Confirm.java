62531bc5e481b0e47d99a8b4fffeb598b816b81d
gii <gn.nsya@gmail.com>
2023-06-30 10:59:13 +0700

    ke 2

---
+++ app/src/main/java/com/example/studio/Activity/Confirm.java
@@ -0,0 +1,16 @@
+package com.example.studio.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.studio.R;
+
+public class Confirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirm);
+    }
+}
---
+++ app/src/main/java/com/example/studio/Activity/ItemHistory.java
@@ -0,0 +1,16 @@
+package com.example.studio.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.studio.R;
+
+public class ItemHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_history);
+    }
+}
